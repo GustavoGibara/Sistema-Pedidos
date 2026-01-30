3a23cc7d4b52965797c49457aa16e6cdd54d8623
Gibara <gustavo.gibara@gmail.com.br>
2026-01-30 14:50:32 -0300

    Criada a classe e enum Status

---
+++ src/entities/Order.java
@@ -0,0 +1,59 @@
+package entities;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import enums.Status;
+
+public class Order {
+    private Long id;
+    private Client client;
+    private List<Product> products = new ArrayList<>();
+    private LocalDate orderDate;
+    private Status status;
+    
+    public Order() {
+    }
+
+    public Order(Long id, Client client, LocalDate orderDate, Status status) {
+        this.id = id;
+        this.client = client;
+        this.orderDate = orderDate;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Client getClient() {
+        return client;
+    }
+    public void setClient(Client client) {
+        this.client = client;
+    }
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+    public Status getStatus() {
+        return status;
+    }
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    @Override
+    public String toString() {
+        return "Order [id=" + id + ", client=" + client + ", orderDate=" + orderDate + ", status=" + status + "]";
+    }
+
+}
---
+++ src/enums/Status.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum Status {
+    ABERTO,
+    PAGO,
+    CANCELADO;
+}
